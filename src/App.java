46ca05f20ae9066bcc802a718bd8023191b0ed4f
Chiara Belfiore <Chiara@example.com>
2024-02-14 19:28:31 +0100

    gattini

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("\n \n");
         System.out.println(Cats.printCat1());
+        System.out.println("\n \n");
+        System.out.println(Cats.printCat2());
+        System.out.println("\n \n");
+        System.out.println(Cats.printCat3());
+
     }
 }
